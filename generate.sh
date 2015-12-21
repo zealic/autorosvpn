7c615c7338004cdada71db21bd26b8020055977c
zealic <zealic@gmail.com>
2015-12-21 12:02:07 +0800

    + Added chnroutes.txt and update list.

--- generate.sh
+++ generate.sh
@@ -5,6 +5,9 @@ echo "/ip firewall address-list" >> $RSC_FILE
 echo "add address=10.0.0.0/8 disabled=no list=novpn" >> $RSC_FILE
 echo "add address=172.16.0.0/12 disabled=no list=novpn" >> $RSC_FILE
 echo "add address=192.168.0.0/16 disabled=no list=novpn" >> $RSC_FILE
-wget -O- 'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' \
+curl 'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' \
   | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
   | awk -F\, '{print "add address=" $0 " disabled=no list=novpn" }' >> $RSC_FILE
+
+curl 'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' \
+  | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chnroutes.txt
