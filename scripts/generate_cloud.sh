50cdcb9235052491594777ad19acdccd027cd56d
zealic <zealic@gmail.com>
2021-11-28 02:37:51 +0800

    improve: Generate cloud service provider routes to directory; and generate all zones route file.

--- scripts/generate_cloud.sh
+++ scripts/generate_cloud.sh
@@ -7,15 +7,15 @@ fi
 
 # Amazon Web Services
 prepare_aws_route(){
-  local url=https://www.gstatic.com/ipranges/cloud.json
+  local url=https://ip-ranges.amazonaws.com/ip-ranges.json
   local file=$1
   local iplist=$2
   local zonelist=$3
 
   curl -fsSL -o $file ${url}
 
-  grep -Po 'Prefix\": "\K([^"]+)' $file > $iplist
-  grep -Po 'scope.+"\K([^"]+)' $file > $zonelist
+  grep -Po 'prefix\": "\K([^"]+)' $file > $iplist
+  grep -Po 'region\": "\K([^"]+)' $file | tr '[:upper:]' '[:lower:]' > $zonelist
 
   if test `cat $iplist | wc -l` -ne `cat $zonelist | wc -l`; then
     echo IP list and zones not equals.
@@ -44,7 +44,7 @@ prepare_gcp_route(){
   curl -fsSL -o $file ${url}
 
   grep -Po 'Prefix\": "\K([^"]+)' $file > $iplist
-  grep -Po 'scope.+"\K([^"]+)' $file > $zonelist
+  grep -Po 'scope\": "\K([^"]+)' $file | tr '[:upper:]' '[:lower:]' > $zonelist
 
   if test `cat $iplist | wc -l` -ne `cat $zonelist | wc -l`; then
     echo IP list and zones not equals.
@@ -59,7 +59,7 @@ make_route(){
   local iplist=$2
   echo "/ip firewall address-list remove [/ip firewall address-list find list=${name}]"
   echo "/ip firewall address-list"
-  cat $iplist \
+  cat $iplist | sort -t . -n \
     | awk -v name=$name '{ print "add address=" $1, "disabled=no", "list=" name }'
 }
 
@@ -67,42 +67,144 @@ make_route_zones(){
   local name=$1
   local iplist=$2
   local zonelist=$3
-  local zonelist_trimed=$3.trimed
-  local merged_file=$1.merged
-  cat $zonelist | grep -Eo '.+[^0-9]' > $zonelist_trimed
-  cat $zonelist_trimed | uniq \
+  local zonelist_trimed=$3.zones.trimed
+  local merged_file=$1.zones.merged
+  cat $zonelist | grep -Eo '^[a-z]+(-[a-z]+){0,}' > $zonelist_trimed
+  if test `cat $zonelist | wc -l` -ne `cat $zonelist_trimed | wc -l`; then
+    echo Bad trim zonelist. >&2
+    exit 1
+  fi
+  cat $zonelist_trimed | sort | uniq \
     | awk -v name=$name '{ print "/ip firewall address-list remove [/ip firewall address-list find list=" name "@" $1 "]"}'
   echo "/ip firewall address-list"
-  paste $iplist $zonelist > $merged_file
+  paste $iplist $zonelist $zonelist_trimed | sort -k2 > $merged_file
 
   while read line; do
   	local fields=($(echo "$line"))
     local address=${fields[0]}
-    local zone=$(echo "${fields[1]}" | grep -Eo '.+[^0-9]')
+    local zone=${fields[2]}
     echo "add address=$address disabled=no list=$name@$zone"
   done < $merged_file
   rm $merged_file $zonelist_trimed
+}
+
+make_route_zones(){
+  local name=$1
+  local iplist=$2
+  local zonelist=$3
+  local zonelist_trimed=$3.zones.trimed
+  local merged_file=$1.zones.merged
+  cat $zonelist | grep -Eo '^[a-z]+(-[a-z]+){0,}' > $zonelist_trimed
+  if test `cat $zonelist | wc -l` -ne `cat $zonelist_trimed | wc -l`; then
+    echo Bad trim zonelist. >&2
+    exit 1
+  fi
+
+  paste $iplist $zonelist $zonelist_trimed | sort -k2 > $merged_file
+
+  # All
+  cat $merged_file |
+  {
+    cat $zonelist_trimed | sort | uniq \
+      | awk -v name=$name '{ print "/ip firewall address-list remove [/ip firewall address-list find list=" name "@" $1 "]"}'
+    echo "/ip firewall address-list"
+    while read line; do
+      local fields=($(echo "$line"))
+      local address=${fields[0]}
+      local zone=${fields[2]}
+      echo "add address=$address disabled=no list=$name@$zone"
+    done
+  } | trim_ipv6 > $name-zones.rsc
+
+  # Zones
+  cat $zonelist_trimed | sort | uniq |
+  {
+    while read zone; do
+      grep -E "${zone}$" $merged_file | write_rsc name@$zone > $name@$zone.rsc
+      grep -E "${zone}$" $merged_file | write_txt > $name@$zone.txt
+    done
+  }
+
+  # Subzones
+  cat $zonelist | sort | uniq |
+  {
+    while read subzone; do
+      grep "${subzone}" $merged_file | write_rsc $subzone > $name@$subzone.rsc
+      grep "${subzone}" $merged_file | write_txt > $name@$subzone.txt
+    done
+  }
+
+  rm $merged_file $zonelist_trimed
+}
+
+make_route_zone(){
+  local zone=$1
+  echo "/ip firewall address-list remove [/ip firewall address-list find list=\"${zone}\""
+  echo "/ip firewall address-list"
+  while read line; do
+    local fields=($(echo "$line"))
+    local address=${fields[0]}
+    echo "add address=$address disabled=no list=$name@$zone"
+  done
 }
 
 make_route_regions(){
   local name=$1
   local iplist=$2
   local zonelist=$3
-  local zonelist_trimed=$3.trimed
-  local merged_file=$1.merged
-  cat $zonelist | grep -Eo '^[^-]+$' > $zonelist_trimed
-  cat $zonelist_trimed | uniq \
-    | awk -v name=$name '{ print "/ip firewall address-list remove [/ip firewall address-list find list=" name "@" $1 "]"}'
-  echo "/ip firewall address-list"
-  paste $iplist $zonelist > $merged_file
+  local zonelist_trimed=$3.regions.trimed
+  local merged_file=$1.regions.merged
+  cat $zonelist | grep -Eo '^[a-z]+' > $zonelist_trimed
+  if test `cat $zonelist | wc -l` -ne `cat $zonelist_trimed | wc -l`; then
+    echo Bad trim zonelist. >&2
+    exit 1
+  fi
 
+  paste $iplist $zonelist $zonelist_trimed | sort -k2 > $merged_file
+
+  # All
+  cat $merged_file |
+  {
+    cat $zonelist_trimed | sort | uniq \
+      | awk -v name=$name '{ print "/ip firewall address-list remove [/ip firewall address-list find list=" name "@" $1 "]"}'
+    echo "/ip firewall address-list"
+    while read line; do
+      local fields=($(echo "$line"))
+      local address=${fields[0]}
+      local region=${fields[2]}
+      echo "add address=$address disabled=no list=$name@$region"
+    done
+  } > $name-regions.rsc
+
+  # Regions
+  cat $zonelist_trimed | sort | uniq |
+  {
+    while read region; do
+      grep -E "${region}$" $merged_file | write_rsc $name@$region > $name@$region.rsc
+      grep -E "${region}$" $merged_file | write_txt > $name@$region.txt
+    done
+  }
+
+  rm $merged_file $zonelist_trimed
+}
+
+write_rsc(){
+  local name=$1
+  echo "/ip firewall address-list remove [/ip firewall address-list find list=\"${name}\""
+  echo "/ip firewall address-list"
   while read line; do
     local fields=($(echo "$line"))
     local address=${fields[0]}
-    local region=$(echo "${fields[1]}" | grep -Eo '^[^-]+')
-    echo "add address=$address disabled=no list=$name@$region"
-  done < $merged_file
-  rm $merged_file $zonelist_trimed
+    echo "add address=$address disabled=no list=$name"
+  done | trim_ipv6
+}
+
+write_txt(){
+  while read line; do
+    local fields=($(echo "$line"))
+    local address=${fields[0]}
+    echo "$address"
+  done | trim_ipv6
 }
 
 trim_ipv6() {
@@ -114,6 +216,14 @@ generate(){
   local provider=$1
   local iplist=${provider}_ips.list
   local zonelist=${provider}_zones.list
+
+  echo Generating $provider routes...
+  if [[ -d $provider ]]; then
+    rm -r $provider
+  fi
+  mkdir $provider
+
+  pushd $provider > /dev/null
   prepare_${provider}_route ${provider}.json $iplist $zonelist
   # IP lists
   cat $iplist | trim_ipv6 | sort -t . -n > route-${provider}.txt
@@ -121,14 +231,16 @@ generate(){
   make_route route-${provider} $iplist | trim_ipv6 > route-${provider}.rsc
   if [[ -f $zonelist ]]; then
     # Regions route
-    make_route_regions route-${provider} $iplist $zonelist | uniq | trim_ipv6 > route-${provider}-regions.rsc
+    make_route_regions route-${provider} $iplist $zonelist
     # Zones route
-    make_route_zones   route-${provider} $iplist $zonelist | trim_ipv6 > route-${provider}-zones.rsc
+    make_route_zones   route-${provider} $iplist $zonelist
     rm $zonelist
   fi
   rm $iplist
+  popd > /dev/null
 }
 
 generate aws
-generate gcp
 generate azure
+generate gcp
+
