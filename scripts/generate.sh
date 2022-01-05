7d8c0d53430c935752f17828fe8cada55d114b59
zealic <zealic@gmail.com>
2022-01-05 15:44:11 +0800

    feat: Support ipv6 for chnroutes.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,3 +2,6 @@
 # chnroutes
 curl -fsSL 'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' \
   | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chnroutes.txt
+
+curl -fsSL 'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' \
+  | awk -F\| '/CN\|ipv6/ { printf("%s/%d\n", $4, $5) }' > chnroutes.ipv6.txt
--- scripts/generate_ros.sh
+++ scripts/generate_ros.sh
@@ -15,10 +15,29 @@ make_ros() {
   done < $file
 }
 
+make_ros_ipv6() {
+  local file=$1
+  local name=$2
+  local lan=$3
+  echo "/ipv6 firewall address-list remove [/ip firewall address-list find list=$name]"
+  echo "/ipv6 firewall address-list"
+  if [[ ! -z "$lan" ]]; then
+    echo "add address=fd00::/8 disabled=no list=$name"
+  fi
+  while read line; do
+    echo "add address=$line disabled=no list=$name"
+  done < $file
+}
+
 for file in *.txt; do
   name=${file%.*}
-  make_ros $file $name > $name.rsc
+  if [ -z ${file##*.ipv6.txt} ]; then
+    make_ros_ipv6 $file $name > $name.rsc
+  else
+    make_ros $file $name > $name.rsc
+  fi
 done
 
 # Generate backward Compatibility address-list.rsc
 make_ros chnroutes.txt novpn yes > address-list.rsc
+make_ros_ipv6 chnroutes.ipv6.txt novpn yes > address-list.ipv6.rsc
